2f54a95d6092e50af1731619916e1d84b172f838
Leonardo Quadros <leonardoq@ciandt.com>
2022-06-02 11:22:36 -0300

    Teste

--- 0x00/email/Pessoa.java
+++ 0x00/email/Pessoa.java
@@ -1,4 +1,9 @@
 package email;
 
 public class Pessoa {
+    private String email;
+
+    public static boolean emailValid(String email) {
+        return (email.length() <= 50 || email.contains("@")) ? true : false;
+    }
 }
