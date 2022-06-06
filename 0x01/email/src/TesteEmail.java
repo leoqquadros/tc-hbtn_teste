c32ab058c04bc04797176022f208bfe3ca655622
Leonardo Quadros <leonardoq@ciandt.com>
2022-06-06 15:04:07 -0300

    Teste

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -1,4 +1,4 @@
-package email.src;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
